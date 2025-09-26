1165632177b79535ef11b4e54649a9891f9cf674
Jonathan P <jonathanphung2005@gmail.com>
2025-09-25 20:29:29 -0700

    working on simple hello world api

---
+++ backend/src/main/java/com/birdie/backend/BirdieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.birdie.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdieBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/birdie/backend/BirdieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
