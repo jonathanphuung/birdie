483286a0718d0c720e8848220ef6f8fae0904a00
Jonathan P <jonathanphung2005@gmail.com>
2025-09-25 21:21:52 -0700

    made the first rest api

---
+++ backend/set-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export SPRING_DATASOURCE_URL="jdbc:postgresql://db.mbcpppbdzoxcveyitegg.supabase.co:5432/postgres?sslmode=require"
+export SPRING_DATASOURCE_USERNAME="postgres"
+export SPRING_DATASOURCE_PASSWORD="Khackhoa2495$"
